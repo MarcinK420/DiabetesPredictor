98839ca29b357838701d8c1b3929357147c8b4cb
MarcinK420 <156346564+MarcinK420@users.noreply.github.com>
2025-10-07 15:41:18 +0200

    Blokada konta po 5 nieudanych probach logowania

--- diabetes_clinic_appointments/authentication/models.py
+++ diabetes_clinic_appointments/authentication/models.py
@@ -1,21 +1,59 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils import timezone
+from datetime import timedelta
 
 class User(AbstractUser):
     USER_TYPE_CHOICES = (
         ('patient', 'Pacjent'),
         ('doctor', 'Lekarz'),
     )
-    
+
     user_type = models.CharField(
-        max_length=10, 
+        max_length=10,
         choices=USER_TYPE_CHOICES,
         default='patient'
     )
     phone_number = models.CharField(max_length=15, blank=True)
-    
+
+    # Pola do śledzenia nieudanych prób logowania
+    failed_login_attempts = models.IntegerField(default=0)
+    last_failed_login = models.DateTimeField(null=True, blank=True)
+    account_locked_until = models.DateTimeField(null=True, blank=True)
+
     def is_patient(self):
         return self.user_type == 'patient'
-    
+
     def is_doctor(self):
         return self.user_type == 'doctor'
+
+    def is_account_locked(self):
+        """Sprawdza czy konto jest zablokowane"""
+        if self.account_locked_until:
+            if timezone.now() < self.account_locked_until:
+                return True
+            else:
+                # Odblokuj konto po upływie czasu
+                self.failed_login_attempts = 0
+                self.account_locked_until = None
+                self.save()
+                return False
+        return False
+
+    def increment_failed_login(self):
+        """Zwiększa licznik nieudanych prób logowania"""
+        self.failed_login_attempts += 1
+        self.last_failed_login = timezone.now()
+
+        if self.failed_login_attempts >= 5:
+            # Blokada na 15 minut po 5 nieudanych próbach
+            self.account_locked_until = timezone.now() + timedelta(minutes=15)
+
+        self.save()
+
+    def reset_failed_login(self):
+        """Resetuje licznik nieudanych prób logowania"""
+        self.failed_login_attempts = 0
+        self.last_failed_login = None
+        self.account_locked_until = None
+        self.save()
--- diabetes_clinic_appointments/authentication/views.py
+++ diabetes_clinic_appointments/authentication/views.py
@@ -5,20 +5,53 @@ from django.contrib import messages
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 from .forms import PatientRegistrationForm, DoctorRegistrationForm
+from .models import User
+from django.utils import timezone
 
 def login_view(request):
     if request.method == 'POST':
+        username = request.POST.get('username')
+
+        # Sprawdź czy użytkownik istnieje
+        try:
+            user = User.objects.get(username=username)
+
+            # Sprawdź czy konto jest zablokowane
+            if user.is_account_locked():
+                time_remaining = (user.account_locked_until - timezone.now()).seconds // 60 + 1
+                messages.error(request, f'Twoje konto jest zablokowane z powodu zbyt wielu nieudanych prób logowania. Spróbuj ponownie za {time_remaining} minut.')
+                return render(request, 'authentication/login.html', {'form': AuthenticationForm()})
+        except User.DoesNotExist:
+            pass
+
         form = AuthenticationForm(request, data=request.POST)
         if form.is_valid():
             user = form.get_user()
+
+            # Resetuj licznik nieudanych prób logowania
+            user.reset_failed_login()
+
             login(request, user)
             if user.is_patient():
                 return redirect('patients:dashboard')
             elif user.is_doctor():
                 return redirect('doctors:dashboard')
+        else:
+            # Zwiększ licznik nieudanych prób logowania
+            try:
+                user = User.objects.get(username=username)
+                user.increment_failed_login()
+
+                remaining_attempts = 5 - user.failed_login_attempts
+                if remaining_attempts > 0:
+                    messages.error(request, f'Nieprawidłowa nazwa użytkownika lub hasło. Pozostało prób: {remaining_attempts}')
+                else:
+                    messages.error(request, 'Twoje konto zostało zablokowane na 15 minut z powodu zbyt wielu nieudanych prób logowania.')
+            except User.DoesNotExist:
+                messages.error(request, 'Nieprawidłowa nazwa użytkownika lub hasło.')
     else:
         form = AuthenticationForm()
-    
+
     return render(request, 'authentication/login.html', {'form': form})
 
 def register_choice(request):
